17ed3be1974c302da6a5cb2b7b3e5d72901730cb
Daniel Allen <dan@aeternagroup.net>
2026-07-27 18:29:35 +0100

    Update to schema version v4.81.0

--- main.tf
+++ main.tf
@@ -84,60 +84,60 @@ locals {
 }
 
 module "dns_zones" {
-  source    = "git::https://github.com/AeternaModules/azurerm_dns_zone.git?ref=v4.80.0"
+  source    = "git::https://github.com/AeternaModules/azurerm_dns_zone.git?ref=v4.81.0"
   dns_zones = local.dns_zones
 }
 
 module "dns_a_records" {
-  source        = "git::https://github.com/AeternaModules/azurerm_dns_a_record.git?ref=v4.80.0"
+  source        = "git::https://github.com/AeternaModules/azurerm_dns_a_record.git?ref=v4.81.0"
   dns_a_records = local.dns_a_records
   depends_on    = [module.dns_zones]
 }
 
 module "dns_aaaa_records" {
-  source           = "git::https://github.com/AeternaModules/azurerm_dns_aaaa_record.git?ref=v4.80.0"
+  source           = "git::https://github.com/AeternaModules/azurerm_dns_aaaa_record.git?ref=v4.81.0"
   dns_aaaa_records = local.dns_aaaa_records
   depends_on       = [module.dns_zones]
 }
 
 module "dns_caa_records" {
-  source          = "git::https://github.com/AeternaModules/azurerm_dns_caa_record.git?ref=v4.80.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_dns_caa_record.git?ref=v4.81.0"
   dns_caa_records = local.dns_caa_records
   depends_on      = [module.dns_zones]
 }
 
 module "dns_cname_records" {
-  source            = "git::https://github.com/AeternaModules/azurerm_dns_cname_record.git?ref=v4.80.0"
+  source            = "git::https://github.com/AeternaModules/azurerm_dns_cname_record.git?ref=v4.81.0"
   dns_cname_records = local.dns_cname_records
   depends_on        = [module.dns_zones]
 }
 
 module "dns_mx_records" {
-  source         = "git::https://github.com/AeternaModules/azurerm_dns_mx_record.git?ref=v4.80.0"
+  source         = "git::https://github.com/AeternaModules/azurerm_dns_mx_record.git?ref=v4.81.0"
   dns_mx_records = local.dns_mx_records
   depends_on     = [module.dns_zones]
 }
 
 module "dns_ns_records" {
-  source         = "git::https://github.com/AeternaModules/azurerm_dns_ns_record.git?ref=v4.80.0"
+  source         = "git::https://github.com/AeternaModules/azurerm_dns_ns_record.git?ref=v4.81.0"
   dns_ns_records = local.dns_ns_records
   depends_on     = [module.dns_zones]
 }
 
 module "dns_ptr_records" {
-  source          = "git::https://github.com/AeternaModules/azurerm_dns_ptr_record.git?ref=v4.80.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_dns_ptr_record.git?ref=v4.81.0"
   dns_ptr_records = local.dns_ptr_records
   depends_on      = [module.dns_zones]
 }
 
 module "dns_srv_records" {
-  source          = "git::https://github.com/AeternaModules/azurerm_dns_srv_record.git?ref=v4.80.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_dns_srv_record.git?ref=v4.81.0"
   dns_srv_records = local.dns_srv_records
   depends_on      = [module.dns_zones]
 }
 
 module "dns_txt_records" {
-  source          = "git::https://github.com/AeternaModules/azurerm_dns_txt_record.git?ref=v4.80.0"
+  source          = "git::https://github.com/AeternaModules/azurerm_dns_txt_record.git?ref=v4.81.0"
   dns_txt_records = local.dns_txt_records
   depends_on      = [module.dns_zones]
 }
